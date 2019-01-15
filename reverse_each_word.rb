d87f49e5e3ab05e409b97a7eb1b7cad449c08fa6
Luis Zabala <luis.zabala076@gmail.com>
2019-01-15 15:33:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence.collect do |rv_sentence_Array|
+  sentence.split.collect do |rv_sentence_Array|
     rv_sentence_Array.reverse.join(" ")
   end
   rv_sentence_Array
