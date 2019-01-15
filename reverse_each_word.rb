211b3c64ec33ff1f4d4c30ed6dac9f637a92852b
Luis Zabala <luis.zabala076@gmail.com>
2019-01-15 15:25:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(reverse)
   word = "Hello there, and how are you?"
-reverse.collect.split {|word| word << reverse.join(" ")}
+reverse.collect.split {|words| words << reverse.join(" ")}
 reverse
 end
 end
